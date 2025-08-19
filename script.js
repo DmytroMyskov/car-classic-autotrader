567176614b1bb33b75f2bf1ef4aaa93314244ef7
Dmytro <coulheyman@gmail.com>
2025-08-19 12:27:11 +0300

    Refactor main content structure in index.html and add styles for FAQ section

---
+++ script.js
@@ -0,0 +1,19 @@
+// const faqItems = document.querySelectorAll(".faq-item");
+
+// faqItems.forEach(item => {
+//   const btn = item.querySelector("button");
+
+//   item.addEventListener("click", () => {
+//     const isActive = item.classList.contains("active");
+
+//     faqItems.forEach(i => {
+//       i.classList.remove("active");
+//       i.querySelector("button").textContent = "+";
+//     });
+
+//     if (!isActive) {
+//       item.classList.add("active");
+//       btn.textContent = "−";
+//     }
+//   });
+// });
